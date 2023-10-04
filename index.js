5c080fbd8a6307c7fa8dbd4a3b90b08c84aaad5f
naruka1023 <naruka1023@yahoo.com>
2023-10-04 14:29:48 +0700

    add new fields

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const readline = require('readline');
 const https = require('https')
+const fs = require('fs')
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,7 +10,10 @@ const rl = readline.createInterface({
 
 
 
+const filePath = 'scrapedData.json';
 
+// Read the JSON file (or create an empty object if the file doesn't exist)
+let jsonData = {};
 (async () => {
     const searchtermMatrix = [
       ['gym', 'Track and Field', 'Boxing', 'Wrestling', 'Judo', 'Taekwondo', 'Karate', 'Muay Thai', 'Kickboxing', 'Brazillian Jiu Jutsu', 'Fencing', 'MMA', 'Krav Maga', 'Tennis', 'Table Tennis', 'Badminton', 'Squash', 'Racquetball', 'Padel', 'Swimming', 'Diving', 'Surfing', 'Windsurfing', 'Kite Surfing', 'Wakeboarding', 'Paddleboarding', 'Canoe', 'Sailing', 'Water Polo', 'Mountain Biking', 'BMX', 'Cyclocross', 'Gravel', 'Dressage', 'Polo', 'Trail Riding', 'Gymkhana', 'Darts', 'Billiards', 'Pool', 'Snooker', 'Bowling', 'Weightlifting', 'Bodybuilding', 'Calisthenics', 'HIIT', 'Pilates', 'Aerobics', 'Skydiving', 'Rock Climbing', 'Mountaineering', 'White-water Rafting', 'Caving', 'Paragliding', 'Hang Gliding', 'Skateboarding', 'Free Diving', 'Highlining', 'Parkour', 'Sandboarding', 'Ballroom Dancing', 'Latin Dancing', 'Tango', 'Salsa', 'Ballet', 'Contemporary', 'Hip-Hop', 'Shuffling', 'Football', 'Basketball', 'Baseball', 'Cricket', 'Hockey', 'Rugby', 'Volleyball', 'Lacrosse', 'Chess', 'Golf'],
@@ -17,7 +21,8 @@ const rl = readline.createInterface({
       ['bangkok', 'kuala lumpur']]
       
     let allCombinations = []
-    allCombinations = generateCombinations(searchtermMatrix)
+    allCombinations = ['personal trainer bangkok', 'gym trainer bangkok']
+    // allCombinations = generateCombinations(searchtermMatrix)
     // allCombinations.splice(0,2)
     
     const browser = await puppeteer.launch({ headless: false,
@@ -28,34 +33,45 @@ const rl = readline.createInterface({
     
     try {
  
+      const fileContent = fs.readFileSync(filePath, 'utf-8');
+      jsonData = JSON.parse(fileContent);
       const context = browser.defaultBrowserContext();
                                   //        URL                  An array of permissions
       context.overridePermissions("https://www.tiktok.com", ["notifications"]);
       const page = await browser.newPage();
       await page.goto(encodeURI(`https://www.tiktok.com`));
-      page.waitForSelector('.efna91q2')
-      const loginButton = await page.$('.efna91q2')
+      // page.waitForSelector('.efna91q2')
+      // const loginButton = await page.$('.efna91q2')
       
-      loginButton.click(),
-      await page.waitForSelector('[data-list-item-value="email/username"]')
-      emailButton = await page.$('[data-list-item-value="email/username"]')
-      emailButton.click()
+      // loginButton.click(),
+      // await page.waitForSelector('[data-list-item-value="email/username"]')
+      // emailButton = await page.$('[data-list-item-value="email/username"]')
+      // emailButton.click()
       
-      await page.waitForSelector('[name="username"]')
+      // await page.waitForSelector('[name="username"]')
 
-      username = await page.$('[name="username"]')
-      await username.type('p.muangsaen@gmail.com', {delay: 100})
-      password = await page.$('.tiktok-15cv7mx-InputContainer')
-      await password.type('W0rld0fw@rcr@ft1!', {delay: 100})
-      signInButton = await page.$('[data-e2e="login-button"]')
-      signInButton.click()
+      // username = await page.$('[name="username"]')
+      // await username.type('p.muangsaen@gmail.com', {delay: 100})
+      // password = await page.$('.tiktok-15cv7mx-InputContainer')
+      // await password.type('W0rld0fw@rcr@ft1!', {delay: 100})
+      // signInButton = await page.$('[data-e2e="login-button"]')
+      // signInButton.click()
       await waitForUserInput('Press Enter to continue...');
 
 
       // this is where the scraping begins 
+    
     for(let i = 0; i < allCombinations.length; i++){
         let payload = await phase1(page, allCombinations[i], i)
-        await phase2(page, payload, allCombinations[i])
+        let value = await phase2(page, payload, allCombinations[i])
+        jsonData[allCombinations[i]] = value;
+
+        // Convert the JSON object back to a JSON string
+        const jsonString = JSON.stringify(jsonData, null, 2); // The '2' adds indentation for better readability
+
+        // Write the JSON string back to the file
+        fs.writeFileSync(filePath, jsonString);
+
         console.log('search term done: ' + i + '/' + allCombinations.length)
     }
   } catch(error){
@@ -99,9 +115,9 @@ async function phase1(page, searchTerm, index){
   await page.goto(encodeURI(`https://www.tiktok.com/search?q=${searchTerm}`));
 
     // Wait for search results to load (you may need to adjust the selector)
-    if(index == 0){
-      await waitForUserInput('Press Enter to continue...');
-    }
+    // if(index == 0){
+    //   await waitForUserInput('Press Enter to continue...');
+    // }
     // await page.waitForSelector('.e19c29qe10');
     await waitForSelectorAsync(page);
     let g = await page.$(".eegew6e0")
@@ -128,21 +144,33 @@ async function phase1(page, searchTerm, index){
 async function phase2(page, payload, searchTerm){
   let innerPayload = payload
   let payloadResult = {}
-  let alreadyexist = 95
-  let notexist = 96
-  for(let i = 151; i < innerPayload.length; i++){
+  let totalPayload = []
+  let alreadyexist = 0
+  let notexist = 0
+  for(let i = 0; i < innerPayload.length; i++){
     await page.goto(`https://www.tiktok.com/@${innerPayload[i]}`);
     // await waitForUserInput('Press Enter to continue...');
     let parsedStatus = {}
-    const imgSrc = await page.$eval('.e1e9er4e1', element => element.src);
+    const imgSrc = await page.$eval('.e1vl87hj2 > .e1e9er4e1', element => element.src);
     const status = await page.$$eval('.e1457k4r1', nodes => nodes.map(n => n.innerText));
     const description = await page.$eval('.e1457k4r3', element => element.innerText);
     const secondaryID = await page.$eval('.ekmpd5l7', element => element.innerText);
-
+    
     status.forEach((s)=>{
       let split = s.split("\n")
-      parsedStatus[split[1]] = split[0]
+      result = 0
+      if(split[0].includes('K')){
+        result = parseInt(split[0].slice(0, -1)) * 1000
+      }else{
+        if(split[0].includes('M')){
+          result = parseInt(split[0].slice(0, -1)) * 1000000
+        }else{
+          result = parseInt(split[0])
+        }
+      }
+      parsedStatus[split[1]] = result
     })
+
     let imageBuffer = await page.goto(imgSrc);
     imageBuffer = await imageBuffer.buffer();
     imageBuffer= imageBuffer.toString('base64')
@@ -157,10 +185,21 @@ async function phase2(page, payload, searchTerm){
       type: "PROFILE",
       channel: "TIKTOK",
       ch_id: innerPayload[i],
-      desc:{
-        String: secondaryID + ' + ' + description,
+      ch_url: {
+        String: `https://www.tiktok.com/@${innerPayload[i]}`,
         Valid: true
       },
+      desc:{
+        String: description,
+        Valid: true
+      },
+      name: {
+        String: secondaryID,
+        Valid: true
+      },
+      following_count: { Int64: parsedStatus.Following, Valid: true },
+      follower_count: { Int64: parsedStatus.Followers, Valid: true },
+      like_count: { Int64: parsedStatus.Likes, Valid: true },
       img_format:  { String: "JPEG", Valid: true },
       img: byteArray
     }
@@ -189,9 +228,11 @@ async function phase2(page, payload, searchTerm){
     req.end();
     
     req.on('error', function(e) {
-      console.error(e);
-    });
+        console.error(e);
+      });
+    totalPayload.push(payloadResult)
   }
+  return totalPayload
 }
 
 function removeDuplicates(arr) {
